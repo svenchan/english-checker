6bf8e39910888fe273495607f50eea0e00bb3569
svenchan <122184120+svenchan@users.noreply.github.com>
2025-10-31 11:39:01 +0900

    Update errors.js fix require

--- api/config/errors.js
+++ api/config/errors.js
@@ -2,7 +2,7 @@
  * Error Messages and HTTP Status Codes
  */
 
-export const ERRORS = {
+const ERRORS = {
   NO_TEXT: 'テキストが必要です',
   NO_CLASS_CODE: 'クラスコードが必要です',
   INVALID_CLASS_CODE: '無効なクラスコードです',
@@ -11,11 +11,16 @@ export const ERRORS = {
   RATE_LIMIT_EXCEEDED: '使いすぎです。少し待ってからもう一度試してください',
 };
 
-export const HTTP_STATUS = {
+const HTTP_STATUS = {
   OK: 200,
   BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
   METHOD_NOT_ALLOWED: 405,
   RATE_LIMIT: 429,
   SERVER_ERROR: 500,
+};
+
+module.exports = {
+  ERRORS,
+  HTTP_STATUS
 };
